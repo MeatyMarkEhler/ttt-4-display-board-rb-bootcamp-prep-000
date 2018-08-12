5707e4e3525e4088c96d18c33c254078c64d0516
Mark Ehler <ehlerm42@gmail.com>
2018-08-12 01:27:06 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,7 +3,7 @@
 
 board = [" "," "," "," "," "," "," "," "," "]
 
-<<<<<<< HEAD
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -11,9 +11,4 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-=======
-def display_board(board){
-  puts " #{board[0]} | #{board[1]} |  #{board[2]} -----------  #{board[3]} |  #{board[4]} |  #{board[5]} -----------  #{board[6]} |  #{board[7]} |  #{board[8]} "
-}
->>>>>>> 3b189968f8dd0b8a4f10b9d445c52766e8b892b2
 
